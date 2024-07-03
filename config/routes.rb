aed2f24e9fbba316018035d93a2b1e30aa305c21
g1tka <165782083+g1tka@users.noreply.github.com>
2024-07-03 11:50:23 +0000

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,36 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {
+    sessions: 'admin/sessions'
+  }
+  devise_for :users, controllers: {
+    registrations: 'user/registrations',
+    sessions: 'user/sessions'
+  }
+
+  root :to => "homes#top"
+  get "/about" => "homes#about"
+  
+  scope module: :user do
+    get 'posts' => 'posts#search', as: 'posts_search'
+    resources :posts do
+      resource :favorites, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]
+    end
+    get 'unsubscribe'
+    patch 'withdraw'
+    resources :users, only: [:index] do
+      get 'search' => 'relationships#search', as: :search
+      resource :relationships, only: [:index, :create, :destroy]
+      get 'followings' => 'relationships#followings', as: 'followings'
+      get 'followers' => 'relationships#followers', as: 'followers'
+    end
+  end
+  
   namespace :admin do
-    get 'comments/index'
+    resources :posts, only: [:index, :show, :update, :destroy]
+    resources :users, only: [:index, :show, :update]
+    patch 'users/:user_id/ban' => 'users#ban', as: 'user_ban'
+    resources :comments, only: [:index, :update, :destroy]
   end
-  namespace :admin do
-    get 'users/index'
-    get 'users/show'
-  end
-  namespace :admin do
-    get 'posts/index'
-    get 'posts/show'
-  end
-  namespace :user do
-    get 'relationships/index'
-  end
-  namespace :user do
-    get 'users/index'
-    get 'users/unsubscribe'
-  end
-  namespace :user do
-    get 'posts/index'
-    get 'posts/new'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  get 'homes/top'
-  get 'homes/about'
-  devise_for :admins
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

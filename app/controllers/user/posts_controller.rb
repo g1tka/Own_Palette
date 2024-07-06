9d2f2be7dd422306d5a27cfb4be2db7311958b02
g1tka <luckycookiedaisukii@gmail.com>
2024-07-06 04:09:17 +0000

    Prepare post new page

--- app/controllers/user/posts_controller.rb
+++ app/controllers/user/posts_controller.rb
@@ -3,11 +3,17 @@ class User::PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def show
   end
 
   def edit
+  end
+  
+  private
+  def post_params
+    params.require(:post).permit(:)
   end
 end

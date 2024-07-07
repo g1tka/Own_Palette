09db3d9c1824233d605f9f2ac852f594cda38cac
g1tka <luckycookiedaisukii@gmail.com>
2024-07-07 11:05:53 +0000

    Add unsubscribe user login message

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -55,15 +55,13 @@ class User::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    posts_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-  def after_sign_up_path_for(resource)
-    posts_path
-  end
+
 end
--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class User::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-
+  before_action :user_state, only: [:create]
   # GET /resource/sign_in
   # def new
   #   super
@@ -17,14 +17,30 @@ class User::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
-
+  def after_sign_in_path_for(resource)
+    posts_path
+  end
+  
+  private
+  
+  def user_state
+    # [userのログインフォームに入力したemailの値]を用いて一致するemailカラムを持つuserを１件特定する。
+    user = User.find_by(email: params[:user][:email])
+    # もしuser=nilがtrueなら（存在しなければ）retuen（user_stateメソッド終了！）。falseなら次の行へ進む。
+    retuen if user.nil?
+    # [userのログインフォームに入力したpasswordの値]と先ほど特定したuserのpasswordが一致しないのであれば、retuen。trueなら次の行へ進む。
+    retuen unless user.valid_password?(params[:user][:password])
+    # ここでようやくis_activeがtrueかfalseかを条件分岐する。falseのときに実行したいことを
+    unless user.is_active
+      flash[:danger] = '退会済のアカウントです。新規登録をお願いします。'
+      redirect_to new_user_session_path
+    end
+  end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
-  def after_sign_in_path_for(resource)
-    posts_path
-  end
+  
 end

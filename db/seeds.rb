640f5b2cf4174ac8d2e0b6bf91675147660fb5b2
g1tka <luckycookiedaisukii@gmail.com>
2024-07-09 06:31:13 +0000

    Fix layouts post index and header

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "seedの実行を開始"
+
+apple = User.find_or_create_by!(email: "apple@example.com") do |user|
+  user.name = "apple"
+  user.password = "password"
+end
+
+bee = User.find_or_create_by!(email: "bee@example.com") do |user|
+  user.name = "bee"
+  user.password = "password"
+end
+
+Post.find_or_create_by!(body: "あかいトマトはとってもおいしそうです。") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post1.jpg"), filename:"sample-post1.jpg")
+  post.color = "red"
+  post.is_open = "true"
+  post.user = apple
+end
+
+Post.find_or_create_by!(body: "きいろいパインは常夏を感じます。") do |post|
+  post.image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/sample-post2.png"), filename:"sample-post1.png")
+  post.color = "yellow"
+  post.is_open = "true"
+  post.user = bee
+end
+
+puts "seedの実行が完了しました"

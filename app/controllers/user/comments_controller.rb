c927daa42669fed3ac9fe911f699c95004dd8519
g1tka <luckycookiedaisukii@gmail.com>
2024-07-13 07:57:16 +0000

    Add filter in comments

--- app/controllers/user/comments_controller.rb
+++ app/controllers/user/comments_controller.rb
@@ -7,7 +7,7 @@ class User::CommentsController < ApplicationController
       # format.html { redirect_to @comment.post }
       # format.js
     else
-      flash[:alert] = @comment.errors.full_messages.join(",")
+      flash[:alert] = @comment.errors.full_messages.join(" / ")
       redirect_back(fallback_location: post_path(params[:post_id]))
       # format.html { redirect_to @comment.post, alert: @comment.errors.full_messages.jpin(",") }
       # format.js
@@ -18,6 +18,13 @@ class User::CommentsController < ApplicationController
     @comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
     @comment.destroy
     redirect_to post_path(params[:post_id])
+  end
+  
+  def filter
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new
+    @comments = @post.comments.where(stance: params[:comment][:stance])
+    render "user/posts/show"
   end
 
   private

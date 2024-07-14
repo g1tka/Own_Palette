c0e92214e8f8a1f69a3c8a0771478bb56b488481
g1tka <luckycookiedaisukii@gmail.com>
2024-07-14 13:07:11 +0000

    Add destroy for comments index

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -2,4 +2,10 @@ class Admin::CommentsController < ApplicationController
   def index
     @posts = Post.all.includes(:comments)
   end
+  
+  def destroy
+    @comment = Comment.find_by(id: params[:id])
+    @comment.destroy
+    redirect_to admin_comments_path
+  end
 end

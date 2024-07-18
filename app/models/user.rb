3cb0bfe56f78bfe008bbd098dee750f8a73b1d1c
g1tka <luckycookiedaisukii@gmail.com>
2024-07-18 07:02:54 +0000

    Prepare controller for followings

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_posts, through: :favorites, source: :post
   has_many :comments, dependent: :destroy
+  
   # 自分がフォローされる（被フォロー）側の関係性
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   # 被フォロー関係を通じて参照→自分をフォローしている人

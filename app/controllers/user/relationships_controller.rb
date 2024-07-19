6ec93478be123716b1657920dfc1436f196f1333
g1tka <luckycookiedaisukii@gmail.com>
2024-07-19 07:16:44 +0000

    Update follow button

--- app/controllers/user/relationships_controller.rb
+++ app/controllers/user/relationships_controller.rb
@@ -5,15 +5,15 @@ class User::RelationshipsController < ApplicationController
   end
   
   def create
-    user = User.find(params[:user_id])
-    current_user.follow(user)
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+    # redirect_to request.referer
   end
   
   def destroy
-    user = User.find(params[:user_id])
-    current_user.unfollow(user)
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    current_user.unfollow(@user)
+    # redirect_to request.referer
   end
   
   def followings

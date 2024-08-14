d3ff9875f91ce73fa3695842153aab05a7bef37d
g1tka <luckycookiedaisukii@gmail.com>
2024-08-14 04:18:53 +0000

    Add sort in admin

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,6 +2,18 @@ class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
   def index
     @users = User.all
+    
+    case params[:sort_by]
+    when "ids"
+      @users = @users.order("id")
+    when "names"
+      @suers = @users.order("name")
+    when "posts"
+      @users = @users.left_joins(:posts).select("users.*, COUNT(posts.id) as posts_count").group(:id).order("posts_count DESC")
+      # lest_joins 投稿数０でも表示するために。select()各ユーザーの投稿数をカウント。users.id, users.name...すべて＊
+    else
+      @users = @users.order(created_at: :desc)
+    end
   end
 
   def user_ban
